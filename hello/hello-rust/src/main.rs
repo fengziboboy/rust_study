b383492694ee637ea4ae868a095e2847484f6d61
jinbozhao <jinbozhao@tencent.com>
2023-07-16 00:26:30 +0800

    函数

--- hello/hello-rust/src/main.rs
+++ hello/hello-rust/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello22, world!");
-}
+    const MAX_POINTS: u32 = 100_00;
+    let mut x = 5;
+    println!("The value of x is:{}", x);
+    x = 6;
+    println!("The value of x is:{}", x);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is {}, const:{}", x, MAX_POINTS);
+
+    let spaces = " ";
+    let spaces = spaces.len();
+    println!("The value of spaces is {}", spaces);
+
+    //let mut spaces1 = " ";
+    //spaces1 = spaces1.len();
+
+     
+    another_function(); 
+} 
+ 
+fn another_function() { 
+    println!("Another function."); 
+} 
+
